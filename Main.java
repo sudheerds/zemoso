c8de73d530b29b89b0518825f2a77819a3e53ac0
sudheerds <sudheerds@ymail.com>
2015-08-13 19:40:00 +0530

    Create Main.java
    the main program for max sum path 2

---
+++ Main.java
@@ -0,0 +1,19 @@
+package assignments;
+
+import java.io.IOException;
+
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		String fname = "/home/sudheerds/Desktop/triangle.txt";
+		Triangle.compute(fname);
+
+	}
+
+}
